4df6de7dbfb5530cf663e615bc1f96a175420a64
User <janylkalykovna>
2022-02-15 14:02:16 +0600

    Оз алдынча даярданган сабактар ооп

---
+++ src/com/company/Cat.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Cat {
+
+    String name;
+    int age;
+    String breed;
+
+    void mymetod() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Cat name is "+name+","+"breed is"+","+breed+","+"cat's age is"+" "+age);
+        }
+
+
+        }
+        void method(){
+            System.out.println("I am very  happy");
+        }
+    }
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Cat mycat =new Cat();
+      mycat.name="Murka";
+      mycat.breed="siam cat";
+      mycat.age=3;
+      mycat.mymetod();
+      mycat.method();
+
+      Cat scat=new Cat();
+      scat.name="Loly";
+      scat.age=1;
+      scat.breed="kyrgyz cat";
+      scat.mymetod();
+      scat.method();
+
+
+
+    }
+}
